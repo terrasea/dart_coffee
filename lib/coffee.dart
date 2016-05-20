4d82d541264c08dda4180920a9631cbd3ddfa1ff
James Hurford <James.Hurford@tangentsnowball.com>
2016-05-20 16:36:41 +0100

    Initial commit for flyweight coffee pattern

---
+++ lib/coffee.dart
@@ -0,0 +1,33 @@
+library flyweight.coffee;
+
+import 'package:reflectable/reflectable.dart';
+
+class IsCoffee extends Reflectable {
+
+}
+
+abstract class Coffee {
+  String get id;
+  String get name;
+  double get price;
+
+  static Map _cache = {};
+  static Map get cache => _cache;
+  Coffee();
+  factory Coffee.get(String name) => _cache.putIfAbsent(name, () => _findCoffee(name));
+
+  static Coffee _findCoffee(String name) {
+    const coffee = const IsCoffee();
+    Map coffees = coffee.annotatedClasses.fold({}, (memo, c) => memo..[c.simpleName] = c);
+
+    var className = name.replaceAll(' ', '');
+
+    return coffees.containsKey(className) ? coffees[className].newInstance('', []) : new FakeCoffee();
+  }
+}
+
+class FakeCoffee implements Coffee {
+  String get id => "FakeCoffee";
+  String get name => 'Unknown Coffee';
+  double get price => 0.0;
+}
